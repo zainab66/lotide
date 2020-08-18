21535a359d295174728f30713301161674772878
zainab66 <zainab66r@yahoo.com>
2020-08-18 08:03:32 +0000

    add head.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = function(array) {
+    return array[0];
+  };
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
