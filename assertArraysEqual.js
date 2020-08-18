fa86155f058a209812ac4c1e02b861e68696772e
zainab66 <zainab66r@yahoo.com>
2020-08-18 22:30:45 +0000

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+  
+const eqArrays = function(arr1, arr2) {
+  let correct = true;
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      correct = false;
+    }
+  }
+  if (arr1.length !== arr2.length) {
+    correct = false;
+  } 
+  
+
+  return correct;
+
+}
+const assertArraysEqual = function(arr1, arr2) {
+
+  if (eqArrays(arr1, arr2)) { 
+    console.log('Arrays are equal!')
+  } else {
+    console.log('Arrays are NOT equal!')
+  }
+
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
