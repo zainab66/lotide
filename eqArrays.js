afcc0368eaa13d9699f4957300c13d8e69147305
zainab66 <zainab66r@yahoo.com>
2020-08-18 22:14:21 +0000

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+  
+const eqArrays = function(arr1, arr2) {
+  let correct = true;
+  for (let i = 0; i < arr1.length; i ++) {
+    if (arr1[i] !== arr2[i]) {
+      correct = false;
+    }
+  }
+  if (arr1.length !== arr2.length) {
+    correct = false;
+  } 
+  
+
+  return correct;
+
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
