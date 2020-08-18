e66b5d9985bcbc3c19cfae85dcaae14179adb645
zainab66 <zainab66r@yahoo.com>
2020-08-18 03:49:56 +0000

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
