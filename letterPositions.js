9815a56e80c122fc652e4f7abaad519ba9a816e6
zainab66 <zainab66r@yahoo.com>
2020-08-19 21:57:45 +0000

    letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -38,7 +38,7 @@ const assertEqual = function(actual, expected) {
     const results = {};
     for (let i = 0; i < sentence.length; i++) {
       if (sentence[i] !== " ") {
-        if (!results[sentence[i]]) results[sentence[i]] = [];
+        if (!results[sentence[i]]) {results[sentence[i]] = [];}
         results[sentence[i]].push(i);
       }
     }
