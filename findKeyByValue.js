cb7d1a48a0da6e7b2d62d2653f3748caabed4f46
zainab66 <zainab66r@yahoo.com>
2020-08-19 22:13:27 +0000

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (value === object[key]) {
+      return key;
+    }
+  }
+};
+
+  
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
