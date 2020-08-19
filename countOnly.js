3aa13cea7ffbefebe79dfc6d4a8c8526200eb194
zainab66 <zainab66r@yahoo.com>
2020-08-19 16:51:51 +0000

    Initial Function Definition

--- countOnly.js
+++ countOnly.js
@@ -9,42 +9,11 @@ const assertEqual = function(actual, expected) {
 
 
   const countOnly = function(allItems, itemsToCount) {
-    const results = {};
-
-    for (const item of allItems) {
-      console.log(item);
-      if (itemsToCount[item]) { 
-        if (results[item]) {
-          results[item] += 1;
-        } else {
-          results[item] = 1;
-        }
-      }
-    }
     
-    return results;
   }
 
 
-  const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
   
-  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
-  
-  assertEqual(result1["Jason"], 1);
-
-  assertEqual(result1["Karima"], undefined);
-  assertEqual(result1["Fang"], 2);
-
 
 
 
@@ -52,6 +21,6 @@ const assertEqual = function(actual, expected) {
 
 
   // TEST CODE
-  //assertEqual("Lighthouse Labs", "Bootcamp");
- // assertEqual(1, 1);
- // assertEqual(1, 2);
+  assertEqual("Lighthouse Labs", "Bootcamp");
+  assertEqual(1, 1);
+  assertEqual(1, 2);
