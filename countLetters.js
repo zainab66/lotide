2fa2aefff00f2a5abdd85aa0cf6b699a199ca2cc
zainab66 <zainab66r@yahoo.com>
2020-08-19 18:02:18 +0000

    update countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,12 @@
 
-  
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+    }
+  };
+    
 const countLetters = function(string) {
   let cleanedString = string.split(" ").join('');
   let results = {};
@@ -13,3 +20,5 @@ const countLetters = function(string) {
   return results;
 }; 
 console.log(countLetters("Lighthouse Labs"));
+assertEqual(countLetters("Lighthouse Labs").g,[1]);
+assertEqual(countLetters("Lighthouse Labs").g,1);
