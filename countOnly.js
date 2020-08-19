afbbc5727a9e8007641fcb95c0445b19e81c62c8
zainab66 <zainab66r@yahoo.com>
2020-08-19 16:57:47 +0000

    Write A Test First

--- countOnly.js
+++ countOnly.js
@@ -12,7 +12,23 @@ const assertEqual = function(actual, expected) {
     
   }
 
-
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+  
+  assertEqual(result1["Jason"], 1);
+  assertEqual(result1["Karima"], undefined);
+  assertEqual(result1["Fang"], 2);
   
 
 
