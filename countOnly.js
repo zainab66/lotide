d874623a492d496de37089a49c378d66991729de
zainab66 <zainab66r@yahoo.com>
2020-08-19 17:00:36 +0000

    Loop over allItems

--- countOnly.js
+++ countOnly.js
@@ -9,7 +9,13 @@ const assertEqual = function(actual, expected) {
 
 
   const countOnly = function(allItems, itemsToCount) {
-    
+    const results = {};
+  
+    for (const item of allItems) {
+      console.log(item);
+    }
+  
+    return results;
   }
 
   const firstNames = [
