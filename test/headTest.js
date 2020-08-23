6ecf686b5bb1bb87faa61e3f0913579e82964385
zainab66 <zainab66r@yahoo.com>
2020-08-23 05:57:00 +0000

    Incorporate Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,12 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual([head([5,6,7])], 5);
-head([5,6,7])
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+ 
+});
