5d03aaa984b3ac5f5ac928c2bcbd5aee1339dea8
zainab66 <zainab66r@yahoo.com>
2020-08-23 05:40:39 +0000

    update

--- head.js
+++ head.js
@@ -4,5 +4,5 @@ const assertEqual = require('./assertEqual');
     return array[0];
   
   };
-  assertEqual([head([5,6,7])], 5);
+  
   module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,2 +1,5 @@
 const head = require('../head');
+const assertEqual = require('../assertEqual');
+
+assertEqual([head([5,6,7])], 5);
 head([5,6,7])
