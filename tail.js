e7fb2488552eb8cb8503e0b00ef6e7ae6c902759
zainab66 <zainab66r@yahoo.com>
2020-08-23 05:43:29 +0000

    update

--- tail.js
+++ tail.js
@@ -9,10 +9,6 @@ const tail = function(array) {
   return tailArray;
 
 };
-  
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
+ 
 module.exports = tail;
 
